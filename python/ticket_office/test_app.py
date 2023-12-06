3251afb8f991278e67a313ab00ac3ef693b63755
LonXag <73125125+LonXag@users.noreply.github.com>
2023-12-06 16:54:24 +0100

    test_app fix 2

--- python/ticket_office/test_app.py
+++ python/ticket_office/test_app.py
@@ -4,9 +4,11 @@ import requests
 train_id = "express_2000"
 session = requests.Session()
 
-def init():
+def init(number = 1):
     response = session.post(f"http://127.0.0.1:8081/reset/{train_id}")
     response.raise_for_status()
+    
+    return reserve(number)
 
 
 def reserve(number = 1):
@@ -22,8 +24,7 @@ def reserve(number = 1):
 
 def test_reserve_seats_from_empty_train():
 
-    init()
-    reservation = reserve().json()
+    reservation = init().json()
     assert reservation["train_id"] == train_id
     assert len(reservation["seats"]) == 4
     assert reservation["seats"] == ["1A", "2A", "3A", "4A"]
@@ -31,8 +32,7 @@ def test_reserve_seats_from_empty_train():
 
 def test_reserve_four_additional_seats():
 
-    init()
-    reservation = reserve(2).json()
+    reservation = init(2).json()
     assert reservation["train_id"] == train_id
     assert len(reservation["seats"]) == 4
     assert reservation["seats"] == ["5A", "6A", "7A", "8A"]
