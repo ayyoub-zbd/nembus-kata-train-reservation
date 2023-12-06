b48880d3424d95fbc4115e0d8ee75db1ee30522b
LonXag <73125125+LonXag@users.noreply.github.com>
2023-12-06 16:48:24 +0100

    refactor test_app.py

--- python/ticket_office/test_app.py
+++ python/ticket_office/test_app.py
@@ -1,42 +1,37 @@
 import json
 import requests
 
+train_id = "express_2000"
+session = requests.Session()
 
-def test_reserve_seats_from_empty_train():
-    train_id = "express_2000"
-
-    session = requests.Session()
+def init():
     response = session.post(f"http://127.0.0.1:8081/reset/{train_id}")
     response.raise_for_status()
 
+
+def reserve():
     response = session.post(
         "http://127.0.0.1:8083/reserve", json={"train_id": train_id, "count": 4}
     )
-
     assert response.status_code == 200, response.text
-    reservation = response.json()
-    assert reservation["train_id"] == "express_2000"
+    
+    return response
+    
+
+def test_reserve_seats_from_empty_train():
+
+    init()
+    reservation = reserve().json()
+    assert reservation["train_id"] == train_id
     assert len(reservation["seats"]) == 4
     assert reservation["seats"] == ["1A", "2A", "3A", "4A"]
 
 
 def test_reserve_four_additional_seats():
-    train_id = "express_2000"
 
-    session = requests.Session()
-    response = session.post(f"http://127.0.0.1:8081/reset/{train_id}")
-    response.raise_for_status()
-
-    response = session.post(
-        "http://127.0.0.1:8083/reserve", json={"train_id": train_id, "count": 4}
-    )
-    assert response.status_code == 200, response.text
-
-    response = session.post(
-        "http://127.0.0.1:8083/reserve", json={"train_id": train_id, "count": 4}
-    )
-    assert response.status_code == 200, response.text
-    reservation = response.json()
-    assert reservation["train_id"] == "express_2000"
+    init()
+    reserve()
+    reservation = reserve().json()
+    assert reservation["train_id"] == train_id
     assert len(reservation["seats"]) == 4
     assert reservation["seats"] == ["5A", "6A", "7A", "8A"]
